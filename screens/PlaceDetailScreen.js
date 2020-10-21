e6f62df5e3104bfa7249e4b0151d0595642d63ec
aydevelop <ay.develop1@gmail.com>
2020-10-21 21:55:02 +0300

    Fix

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
 
 const PlaceDetailScreen = ({ route, navigation }) => {
   React.useLayoutEffect(() => {
@@ -9,8 +10,32 @@ const PlaceDetailScreen = ({ route, navigation }) => {
   })
 
   return (
-    <View>
-      <Text>PlaceDetailScreen</Text>
+    <View style={{ flex: 1, padding: 15 }}>
+      {route.params.place.imageUri && (
+        <Image
+          source={{ uri: route.params.place.imageUri }}
+          style={{ height: '50%' }}
+        />
+      )}
+      {route.params.place.lat > 0 && route.params.place.lng > 0 && (
+        <MapView
+          style={{ height: '50%' }}
+          region={{
+            latitude: route.params.place.lat,
+            longitude: route.params.place.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            title='Picked Location'
+            coordinate={{
+              latitude: route.params.place.lat,
+              longitude: route.params.place.lng,
+            }}
+          />
+        </MapView>
+      )}
     </View>
   )
 }
